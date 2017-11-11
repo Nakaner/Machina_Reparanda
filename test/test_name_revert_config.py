c3967c70661029faf6e6af1a3c00e63222624707
Michael <nakaner@gmx.net>
2017-11-11 22:30:02 +0100

    add tests for conflict solution

--- test/test_name_revert_config.py
+++ test/test_name_revert_config.py
@@ -16,3 +16,23 @@ class NameRevertTestCase(unittest.TestCase):
         result = self.revert_impl.handle_obj(latest)
         self.assertEqual(previous.tags["name"], result.tags["name"])
         self.assertEqual(result.version, 4)
+
+    def test_already_reverted(self):
+        code, v5 = self.data_source.get_version("way", 33072216, 5)
+        code, v6 = self.data_source.get_version("way", 33072216, 6)
+        code, v7 = self.data_source.get_version("way", 33072216, 7)
+        code, latest = self.data_source.get_latest_version("way", 33072216)
+        result = self.revert_impl.solve_conflict(v5, latest, [6])
+        self.assertIsNone(result)
+
+    def test_real_conflict(self):
+        fake_source = MockDataProvider(self.config, fake_data=True)
+        revert_impl = RevertImplementation(self.config, fake_source)
+        code, v5 = fake_source.get_version("way", 33072216, 5)
+        code, v6 = fake_source.get_version("way", 33072216, 6)
+        code, latest = fake_source.get_latest_version("way", 33072216)
+        result = revert_impl.solve_conflict(v5, latest, [6])
+        self.assertIsNotNone(result)
+        self.assertEqual(v5.tags["name"], result.tags["name"])
+        self.assertEqual(result.version, 7)
+        self.assertIn("surface", result.tags)
