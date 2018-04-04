321f0ba0850fa87981c3db3e1a50659fb037446e
Michael Reichert <nakaner@gmx.net>
2018-04-04 22:31:09 +0200

    use correct name of the framework

--- machina_reparanda/abstract_revert_implementation.py
+++ machina_reparanda/abstract_revert_implementation.py
@@ -3,18 +3,18 @@
 
 This file is part of Machina Reparanda.
 
-osmi_simple_views is free software: you can redistribute it and/or modify
+Machina Reparanda is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
 (at your option) any later version.
 
-osmi_simple_views is distributed in the hope that it will be useful,
+Machina Reparanda is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with osmi_simple_views. If not, see <http://www.gnu.org/licenses/>.
+along with Machina Reparanda. If not, see <http://www.gnu.org/licenses/>.
 """
 
 from .sort_functions import obj_to_str
