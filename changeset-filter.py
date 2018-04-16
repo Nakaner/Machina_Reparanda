43b456d00e60488054ff29b74885f98978ca8aef
Michael Reichert <nakaner@gmx.net>
2018-04-16 23:11:36 +0200

    download changeset content directly from API

--- changeset-filter.py
+++ changeset-filter.py
@@ -20,6 +20,7 @@ along with Machina Reparanda. If not, see <http://www.gnu.org/licenses/>.
 """
 
 import sys
+import os.path
 import re
 import datetime
 import argparse
@@ -32,10 +33,11 @@ DATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
 
 
 class CSHandler(osmium.SimpleHandler):
-    def __init__(self, pattern=None, keys_of_interest=None):
+    def __init__(self, osc_output_dir=None, pattern=None, keys_of_interest=None):
         osmium.SimpleHandler.__init__(self)
         self.pattern = pattern
         self.keys_of_interest = keys_of_interest
+        self.osc_output_dir = osc_output_dir
         self.last_timestamp = datetime.datetime.utcnow()
         self.count = 0
 
@@ -43,7 +45,30 @@ class CSHandler(osmium.SimpleHandler):
         self.count = 0
 
     def _output(self, changeset):
-        sys.stdout.write("{}\n".format(changeset.id))
+        if self.osc_output_dir is None:
+            sys.stdout.write("{}\n".format(changeset.id))
+        else:
+            self._download_osc(changeset.id)
+
+    def _download_osc(self, cs_id):
+        file_name = "c{}.osc".format(cs_id)
+        dest_file_path = os.path.join(self.osc_output_dir, file_name)
+        try:
+            url = "{}changeset/{}/download".format(API_URL, cs_id)
+            sys.stderr.write("fetching OSC file from {} ...".format(url))
+            r = requests.get("{}changeset/{}/download".format(API_URL, cs_id), timeout=300, stream=True)
+            if r.status_code != 200:
+                sys.stderr.write("Failed to fetch the contents of changeset {}: {}\n".format(cs_id, r.raise_for_status()))
+                exit(1)
+        except requests.exceptions.Timeout as err:
+            sys.stderr.write("Failed to fetch the contents of changeset {}: {}\n".format(cs_id, err))
+        try:
+            with open(dest_file_path, "wb") as oscfile:
+                oscfile.write(r.content)
+            sys.stderr.write(" {}\n".format(r.status_code))
+        except Exeption as err:
+            sys.stderr.write("Failed to write OSC file: {}\n".format(err))
+            exit(1)
 
     def changeset(self, cs):
         self.last_timestamp = cs.created_at
@@ -84,7 +109,9 @@ class CSDownloader():
         self.handler.reset()
         sys.stderr.write("downloading changeset list, changesets before {}\n".format(self.since.strftime(DATE_FORMAT)))
         url = "{}changesets?{}".format(API_URL, self._query_string())
+        sys.stderr.write("{} ...".format(url))
         r = requests.get(url)
+        sys.stderr.write(" {}\n".format(r.status_code))
         if r.status_code == 200:
             self.handler.apply_buffer(r.content, "osm")
             self.date_to = self.handler.last_timestamp
@@ -96,11 +123,12 @@ parser.add_argument("-v", "--invert", help="invert match", action="store_true", 
 parser.add_argument("-r", "--regex", help="filter comment by regex")
 parser.add_argument("-k", "--keys", help="comma separated list of changeset tag keys to compare with the regular expression", default="comment")
 parser.add_argument("-i", "--ignore-case", help="don't filter case sensitive", action="store_true", default=False)
-parser.add_argument("-d", "--download", help="download the list of changesets from the OSM API", action="store_true", default=False)
+parser.add_argument("-d", "--download-list", help="download the list of changesets from the OSM API", action="store_true", default=False)
 parser.add_argument("--download-user", help="name of the user whose changesets should be downloaded")
 parser.add_argument("--download-uid", help="UID of the user whose changesets should be downloaded", type=int)
 parser.add_argument("--download-since", help="oldest timestamp for changesets to be downloaded, format: YYYY-mm-ddTHH:MM:SS")
 parser.add_argument("--download-to", help="newest timestamp for changesets to be downloaded, format: YYYY-mm-ddTHH:MM:SS", default=datetime.datetime.utcnow().strftime(DATE_FORMAT))
+parser.add_argument("-o", "--osc-output-dir", help="output directory for downloaded content of the changesets (OSC format)", type=str, default=None)
 parser.add_argument("-f", "--input-file", help="XML file containing changeset metadata", type=str, default=None)
 args = parser.parse_args()
 
@@ -108,12 +136,12 @@ if args.regex:
     flags = re.IGNORECASE if args.ignore_case else 0
     pattern = re.compile(args.regex, flags)
     keys_to_search = args.keys.split(",")
-    handler = CSHandler(pattern, keys_to_search)
+    handler = CSHandler(args.osc_output_dir, pattern, keys_to_search)
 else:
-    handler = CSHandler()
+    handler = CSHandler(args.osc_output_dir)
 
 
-if args.download:
+if args.download_list:
     downloader = CSDownloader(args.download_user, args.download_uid, args.download_since, args.download_to, handler)
     while True:
         if not downloader.next():
